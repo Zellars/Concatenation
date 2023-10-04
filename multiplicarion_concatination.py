6487f9c66b1648dd7e7e55bd4babcdb1ddfee593
Zellars <143442920+Zellars@users.noreply.github.com>
2023-10-04 14:23:08 -0400

    Add files via upload

---
+++ multiplicarion_concatination.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+num3 = float(input("Enter third number:"))
+
+product = num1 * num2 * num3
+
+product = str(product)
+print("The product is:" + (product))
